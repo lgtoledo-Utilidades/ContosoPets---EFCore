45ba6f1133947f24b8a7e6427dae15bff7d79491
Leandro Toledo <lgtoledo@outlook.com>
2020-06-09 13:00:07 -0300

    Commit inicial

---
+++ modules/persist-data-ef-core/src/ContosoPets.Domain/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ContosoPets.Domain.Models
+{
+    public partial class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+        public ICollection<ProductOrder> ProductOrders { get; set; }
+    }
+    
+}
---
+++ modules/persist-data-ef-core/src/ContosoPets.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContosoPets.Domain.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public ICollection<ProductOrder> ProductOrders { get; set; }
+    }
+}
